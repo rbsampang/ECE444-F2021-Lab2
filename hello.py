fcf469018b8c640dc98d13009b8c7e8a6998f22a
rbsampang <rbsam99@gmail.com>
2021-09-25 13:36:42 -0400

    add UofT email field to form

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 from datetime import datetime
 
 app = Flask(__name__)
@@ -14,6 +14,7 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
   name = StringField('What is your name?', validators=[DataRequired()])
+  email = StringField('What is your UofT email address?', validators=[DataRequired(), Email()])
   submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET','POST'])
@@ -23,9 +24,13 @@ def index():
     old_name = session.get('name')
     if (old_name is not None and old_name != form.name.data):
       flash('Looks like you changed your name!')
+    old_email = session.get('email')
+    if (old_email is not None and old_email != form.email.data):
+      flash('Looks like you changed your email!')
     session['name'] = form.name.data
+    session['email'] = form.email.data
     return redirect(url_for('index'))
-  return render_template('index.html', form=form, name=session.get('name'))
+  return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
 
 @app.route('/user/<name>')
 def user(name):
